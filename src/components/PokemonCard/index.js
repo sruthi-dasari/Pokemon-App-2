20d096aa6d40158d9ab286a4aa8e76ded77a5f22
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2023-03-16 20:51:55 +0530

    Update index.js

--- src/components/PokemonCard/index.js
+++ src/components/PokemonCard/index.js
@@ -9,29 +9,39 @@ class PokemonCard extends Component {
   // this.gotoPokemonCardDetail();
   //   }
 
-  onClickCard = () => {
-    console.log("In onClickCard()");
-    this.renderPokemonCardDetail();
-  };
+  //   onClickCard = () => {
+  //     console.log("In onClickCard()");
+  //     this.renderPokemonCardDetail();
+  //   };
 
-  renderPokemonCardDetail = () => {
-    console.log("In renderPokemonCardDetail()");
-    const { itemDetail } = this.props;
-    const { url } = itemDetail;
-    return <PokemonCardDetail url={url} />;
-  };
+  //   renderPokemonCardDetail = () => {
+  //     console.log("In renderPokemonCardDetail()");
+  //     const { itemDetail } = this.props;
+  //     const { url } = itemDetail;
+  //     return <PokemonCardDetail url={url} />;
+  //   };
 
   render() {
-    const { itemDetail } = this.props;
-    const { name } = itemDetail;
+    // const { itemDetail } = this.props;
+    // const { name } = itemDetail;
     // console.log(itemDetail);
 
     return (
-      <button type="button" onClick={this.onClickCard}>
-        <div className="pokemon-card-container">
-          <h1 className="pokemon-name">{name}</h1>
+      //   <button type="button" onClick={this.onClickCard}>
+      //     <div className="pokemon-card-container">
+      //       <h1 className="pokemon-name">{name}</h1>
+      //     </div>
+      //   </button>
+      <div className="pokemon-card-container">
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png"
+          alt="pokemon"
+        />
+        <div className="name-and-id-container">
+          <h1 className="pokemon-name">bulbasaur</h1>
+          <p>#1</p>
         </div>
-      </button>
+      </div>
     );
   }
 }
